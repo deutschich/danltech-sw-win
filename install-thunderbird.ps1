187f616c54cf1125cd339db44fc34183c33b36ca
D.L. <140229250+deutschich@users.noreply.github.com>
2026-05-29 21:11:35 +0200

    Replace Invoke-WebRequest with curl for download

--- install-thunderbird.ps1
+++ install-thunderbird.ps1
@@ -1,1 +1,7 @@
-Invoke-WebRequest 'https://download.mozilla.org/?product=thunderbird-msi-latest-ssl&os=win64&lang=de' -OutFile $env:TEMP\setup.msi; Start-Process msiexec.exe -ArgumentList '/i',\"$env:TEMP\setup.msi\",'/quiet','/norestart' -Wait
+curl.exe -L `
+  "https://download.mozilla.org/?product=thunderbird-msi-latest-ssl&os=win64&lang=de" `
+  -o "$env:TEMP\setup.msi"
+
+Start-Process msiexec.exe `
+  -ArgumentList '/i',"$env:TEMP\setup.msi",'/quiet','/norestart' `
+  -Wait
